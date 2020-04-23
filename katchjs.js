2294eac03292ce872bf54e78f5e879214a7167ac
Henrique Inonhe <henriqueinonhe@gmail.com>
2020-04-23 09:29:07 -0300

    Remake

---
+++ katchjs.js
@@ -0,0 +1,80 @@
+katchTestCount = 0;
+katchPassCount = 0;
+katchFailCount = 0;
+
+function RUN_KATCH(testSuite)
+{
+    console.time("Katch Timer");
+    testSuite();
+    console.timeEnd("Katch Timer");
+    console.log("Tests Summary: " + katchTestCount + " Pass: " + katchPassCount + " Fail: " + katchFailCount);
+}
+
+function CHECK(lhs, rhs)
+{
+    /* WEIRD CODE BEGIN 
+     * The following piece of code is an aritificial way to 
+     * overload the "CHECK" function, so it can 
+     * either take two arguments and compare them
+     * or take a single argument which is expected to be a
+     * boolean.
+     *
+     * The reason it has been written this way 
+     * is due to the necessity of quickly inspection of lhs and rhs values
+     * when the assertion fails and it is not using boolean values.
+     */
+    rhs = rhs === undefined ? true : rhs;
+    /* WEIRD CODE END */
+    
+    katchTestCount++;
+
+    const testPass = eval(lhs + " === " + rhs);
+
+    if(testPass)
+    {
+        katchPassCount++;
+    }
+    else
+    {
+        katchFailCount++;
+        console.log(`Test number ${katchTestCount}, "CHECK(${lhs}, ${rhs}")" FAILED!\n
+                     Failed with: "${eval(lhs)} === ${eval(rhs)}"`);
+    }
+}
+
+
+function CHECK_NOTHROW(assertionString)
+{
+    katchTestCount++;
+
+    try
+    {
+        eval(assertionString);
+    }
+    catch(err)
+    {
+        katchFailCount++;
+        console.log(`Test number ${katchTestCount}, "CHECK_NOTHROW(${assertionString})"\n FAILED with exception: ${err}`);
+        return;
+    }
+
+    katchPassCount++;
+}
+
+function CHECK_THROWS(assertionString)
+{
+    katchTestCount++;
+
+    try
+    {
+        eval(assertionString);
+    }
+    catch(err)
+    {
+        katchPassCount++;
+        return;
+    }
+
+    katchFailCount++;
+    console.log(`Test number ${katchTestCount}, "CHECK_THROWS(${assertionString})" FAILED!`);
+}
